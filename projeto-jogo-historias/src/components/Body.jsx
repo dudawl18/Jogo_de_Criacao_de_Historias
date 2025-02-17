6b8cdef47ce7023502727674ee841bf14ac16efc
dudawl18 <mwolfluiz123a3@gmail.com>
2025-02-17 17:29:44 -0300

    continuando o projeto

--- projeto-jogo-historias/src/components/Body.jsx
+++ projeto-jogo-historias/src/components/Body.jsx
@@ -13,13 +13,20 @@ function Body() {
     return (
 
         <div className='body-content'>
-            <div className="telaJogo-content">
+            <div className="telaJogoPart1-content">
                 <p className='primeiroTitulo'>Boas vindas </p>
-                <p className='temaTitulo'>Tema Geral: Naufrágio</p> 
+                <p className='temaTitulo'>Tema Geral: Naufrágio</p>
                 <p className='instrucaoP'>Clique no botão para gerar uma imagem!!</p>
                 <button onClick={SortearImg} className='buttonSortear'>Gerar imagem</button>
+            </div>
+            <div className="telaJogoPart2-content">
                 <div className="img-content">
-                      
+                   <img className='imagemSorteada' src="./public/img/chave.svg" alt="" />
+                   <img className='imagemSorteada' src="./public/img/cobra.svg" alt="" />
+                   <img className='imagemSorteada' src="./public/img/castelo.svg" alt="" />
+                   <img className='imagemSorteada' src="./public/img/arvore.svg" alt="" />
+                   <img className='imagemSorteada' src="./public/img/lanca.svg" alt="" />
+                   <img className='imagemSorteada' src="./public/img/veleiro.svg" alt="" />
                 </div>
             </div>
         </div>
